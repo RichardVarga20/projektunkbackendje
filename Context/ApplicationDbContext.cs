b2adfc9b7b6d8bf6c9c8c2f515fbb5371e4c3a47
Ricsi <ryandesigns302@gmail.com>
2024-12-19 09:45:44 +0100

    Initial commit

---
+++ Context/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using dreamStayHotel.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace dreamStayHotel.Context
+{
+    public class ApplicationDbContext(DbContextOptions options) : IdentityDbContext<ApplicationUser>(options)
+    {
+       public DbSet<Room> Rooms { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Explicitly define the primary key for the 'Room' entity
+            modelBuilder.Entity<Room>().HasKey(r => r.Id);
+        }
+    }
+}
---
+++ Entities/Room.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace dreamStayHotel.Entities
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string[]? Extras { get; set; }
+        public decimal PricePerNight { get; set; }
+        public int MaxAdults { get; set; }
+        public int MaxChildren { get; set; }
+        public string? Img_Src { get; set; }
+        public string? Description { get; set; }
+        public int Rating { get; set; }
+        public DateTime? CheckInDate { get; set; } = null;
+
+    }
+}
